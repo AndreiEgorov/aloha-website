cd437c43437f1ae10b875dd0d3adf1f43c04ca07
Andrei Egorov <andreyegorov1992@gmail.com>
2018-04-18 10:46:50 -0700

    Added a JS file

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+console.log("this works");
+
+//prompt("Hello");
+
+newFunction();
+}
+
+function newFunction() {
+    var mood = prompt("How are you"), { prompt = ("How are you?") };
+    console.log("I am " + mood + " too!");
+}
+newFunction();
+
