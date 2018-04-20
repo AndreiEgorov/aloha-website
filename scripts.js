28ec26a4f72c11f32c61bade3a38192438a85bb9
Andrei Egorov <andreyegorov1992@gmail.com>
2018-04-19 20:46:57 -0700

    Added carousel

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
+$(document).ready(function (){
 
+    $(".carousel").flickity({
+        // options
+        freeScroll: false,
+        wrapAround: true,
+        groupCells: true,
+        // freeScroll: true,
+        contain: true,
+        // disable previous & next buttons and dots
+        prevNextButtons: false,
+        
+
+      });
+
+});
