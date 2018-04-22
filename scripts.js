2433929c3b5702f5c25ddb189ffed35b41d468dc
Andrei Egorov <andreyegorov1992@gmail.com>
2018-04-21 22:15:36 -0700

    finished @media 600px) git status

--- scripts.js
+++ scripts.js
@@ -13,4 +13,124 @@ $(document).ready(function (){
 
       });
 
+
+
+ // Source: https://css-tricks.com/snippets/jquery/smooth-scrolling/#article-header-id-1
+ $('a[href*="#"]')
+ // Remove links that don't actually link to anything
+ .not('[href="#"]')
+ .not('[href="#0"]')
+ .click(function(event) {
+   // On-page links
+   if (
+     location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+     && 
+     location.hostname == this.hostname
+   ) {
+     // Figure out element to scroll to
+     var target = $(this.hash);
+     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+     // Does a scroll target exist?
+     if (target.length) {
+       // Only prevent default if animation is actually gonna happen
+       event.preventDefault();
+       $('html, body').animate({
+         scrollTop: target.offset().top
+       }, 1000, function() {
+         // Callback after animation
+         // Must change focus!
+         var $target = $(target);
+         $target.focus();
+         if ($target.is(":focus")) { // Checking if the target was focused
+           return false;
+         } else {
+           $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+           $target.focus(); // Set focus again
+         };
+       });
+     }
+   }
+ });
+
+
+
+
+
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $('a[href*="#"]')
+// .not('[href="#"]')
+// .not('[href="#0"]')
+// .click(function(event) {
+//     var headerNav=$(".head").height();
+//   // On-page links
+//   if (
+//     location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+//     && 
+//     location.hostname == this.hostname
+//   ) {
+//     // Figure out element to scroll to
+//     var target = $(this.hash);
+//     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+//     // Does a scroll target exist?
+//     if (target.length) {
+//       // Only prevent default if animation is actually gonna happen
+//       event.preventDefault();
+//       $('html, body').animate(
+          
+//         {
+//         scrollTop: target.offset().top - headerNav
+//          }, 
+//          1000, 
+//          function() {
+
+
+
+
+
+//         // Callback after animation
+//         // Must change focus!
+//         var $target = $(target);
+//         $target.focus();
+//         if ($target.is(":focus")) { // Checking if the target was focused
+//           return false;
+//         } else {
+//           $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+//           $target.focus(); // Set focus again
+//         };
+//       });
+//     }
+//   }
+// });
+   
